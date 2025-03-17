9e3a577ee77784309909934e07dd30a5a167ed7d
David Tümmers <dat@connext.de>
2025-03-17 15:04:04 +0100

    stuff

--- src/app/Components/Dialogs/anforderung-dialog/anforderung-dialog.component.ts
+++ src/app/Components/Dialogs/anforderung-dialog/anforderung-dialog.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AnforderungDialogViewModel} from '../../../Models/ViewModels/AnforderungDialogViewModel';
 import {IAnforderung} from '../../../Models/Interfaces/IAnforderung';
 import {FormsModule} from '@angular/forms';
@@ -11,14 +11,18 @@ import {FormsModule} from '@angular/forms';
   templateUrl: './anforderung-dialog.component.html',
   styleUrl: './anforderung-dialog.component.css'
 })
-export class AnforderungDialogComponent {
+export class AnforderungDialogComponent implements OnInit {
 
   @Input() viewModel?: AnforderungDialogViewModel;
-  newOrEditedAnforderung: IAnforderung;
+  oldAnforderung!: IAnforderung;
   isSaveButtonDisabled: boolean = true;
 
   constructor() {
-    this.newOrEditedAnforderung = {
+
+  }
+
+  ngOnInit() {
+    this.oldAnforderung = {
       id: this.viewModel?.anforderung?.id ?? -1,
       data: {
         title: this.viewModel?.anforderung?.data.title ?? "",
@@ -37,7 +41,7 @@ export class AnforderungDialogComponent {
   }
 
   onBtnSaveClicked() {
-    this.viewModel?.onSaveClick(this.newOrEditedAnforderung);
+    this.viewModel?.onSaveClick(this.viewModel?.anforderung!);
   }
 
   onBtnCancleClicked() {
@@ -51,10 +55,10 @@ export class AnforderungDialogComponent {
   }
 
   private hasChanged() {
-    if(this.viewModel?.anforderung?.data.title !== this.newOrEditedAnforderung.data.title) {
+    if(this.viewModel?.anforderung?.data.title !== this.oldAnforderung.data.title) {
       return true;
     }
-    return this.viewModel?.anforderung?.data.beschreibung !== this.newOrEditedAnforderung.data.beschreibung;
+    return this.viewModel?.anforderung?.data.beschreibung !== this.oldAnforderung.data.beschreibung;
   }
 
 }
