6927bc833cf9c2474b0158790f3a5cc362c53a62
David Tümmers <dat@connext.de>
2025-03-20 13:54:31 +0100

    master-database

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 // Firebase-Konfiguration
-const firebaseConfig = {
+const firebaseConfigDeployed = {
+  apiKey: "AIzaSyDZvjeHdVKyWw6CPqu7x-MwM-lho3Fqx7s",
+  authDomain: "uebungsprojekt-deployed.firebaseapp.com",
+  projectId: "uebungsprojekt-deployed",
+  storageBucket: "uebungsprojekt-deployed.firebasestorage.app",
+  messagingSenderId: "401943165348",
+  appId: "1:401943165348:web:8685df2fb8fe2cce46a931"
+};
+
+const firebaseConfigDevelop = {
   apiKey: "AIzaSyBASrddyr2HHBTkga4Dsjk8yED29gptvec",
   authDomain: "uebungsprojekt-52c2a.firebaseapp.com",
   projectId: "uebungsprojekt-52c2a",
@@ -12,10 +21,12 @@ const firebaseConfig = {
   appId: "1:1013875983053:web:4190360a80226652449f84"
 };
 
+const deploy = true;
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(deploy ? firebaseConfigDeployed ? firebaseConfigDevelop)),
     provideFirestore(() => getFirestore()) // 🔥 Firestore als Provider hinzufügen
   ]
 };
